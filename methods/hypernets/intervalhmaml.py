15712719b2aec19bb34114b2b16f9524d76b0b94
Szymon Dziedzic <sz.dziedz@gmail.com>
2024-07-31 00:02:45 +0200

    fix

--- methods/hypernets/intervalhmaml.py
+++ methods/hypernets/intervalhmaml.py
@@ -668,7 +668,7 @@ class IntervalHMAML(HyperMAML):
         metrics["loss_worst_case"] = worst_case_acc_mean
 
         metrics["curr_eps"] = self.eps
-        metrics["curr_radius"] = self.classifier[-1].radius
+        metrics["curr_radius"] = self.classifier.weight.radius
 
         if self.hn_adaptation_strategy == "increasing_alpha":
             metrics["alpha"] = self.alpha
